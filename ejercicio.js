efc82775e299ec977182a2273f88a9b6efc38f74
tom-jus <101270851+tom-jus@users.noreply.github.com>
2022-08-25 14:20:39 -0300

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,11 @@
+
+let altura = 179;
+let altura_2 = 1.79;
+let peso = 77.7;
+let max_valor = "el maximo valor que se puede obtener en Javascript +1"
+let max_valor_JS = Number.MAX_VALUE;
+
+Math.round(altura_2);
+Math.floor(peso);
+max_valor = max_valor_JS
+
